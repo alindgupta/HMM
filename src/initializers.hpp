ae2995be5405c7a37a7a483761ee06edf0cd0109
Alind Gupta <alindgupta19@gmail.com>
2018-03-24 21:31:56 -0400

    Add documentation and namespace scoping for initializers

--- src/initializers.hpp
+++ src/initializers.hpp
@@ -9,59 +9,96 @@
 using MatrixXd = Eigen::MatrixXd;
 using MatrixXdType = Eigen::Ref<const Eigen::MatrixXd>;
 
-// random number generator
-static boost::mt19937 rng;
+namespace hmm {
+  namespace initializers {
+    
+    // random number generator
+    static boost::mt19937 rng;
 
-struct Initializer {
-public:
-  virtual MatrixXdType operator()() = 0;
-};
+    /**
+     * Base initializer class (virtual).
+     * Subclasses should be implemented as functors
+     * templated by parameters.
+     * Calling the functor should return an Eigen matrix.
+     *
+     */
+    struct Initializer {
+    public:
+      virtual MatrixXdType operator()() = 0;
+    };
 
-template<std::size_t n_rows, std::size_t n_cols>
-struct ZeroInitializer : public Initializer {
-public:
-  MatrixXdType operator()() override {
-    return MatrixXd::Zero(n_rows, n_cols);
+
+    /**
+     * Initialize a matrix of zeros.
+     * @tparam n_rows Number of rows
+     * @tparam n_cols Number of columns
+     *
+     */
+    template<std::size_t n_rows, std::size_t n_cols>
+    struct ZeroInitializer : public Initializer {
+    public:
+      MatrixXdType operator()() override {
+        return MatrixXd::Zero(n_rows, n_cols);
+      }
+    };
+
+    
+    /**
+     * Initialize a matrix with uniformly distributed values.
+     * @tparam n_rows Number of rows
+     * @tparam n_cols Number of columns
+     * @param lower Support lower bound for uniform distribution
+     * @param upper Support upper bound for uniform distribution
+     *
+     */
+    template<std::size_t n_rows, std::size_t n_cols>
+    struct UniformInitializer : public Initializer {
+    public:
+      UniformInitializer(const double lower, const double upper)
+        : m_lower(lower),
+          m_upper(upper),
+          m_unifd(boost::random::uniform_real_distribution<>(lower, upper)) {}
+
+      MatrixXdType operator()() override {
+        return MatrixXd::Zero(n_rows, n_cols)
+          .unaryExpr([&](double t){ return m_unifd(rng); });
+      }
+
+    private:
+      double m_lower;
+      double m_upper;
+      boost::random::uniform_real_distribution<double> m_unifd;
+    };
+
+
+    /**
+     * Initialize a matrix with normally distributed values.
+     * @tparam n_rows Number of rows
+     * @tparam n_cols Number of columns
+     * @param mean Location parameter for the normal distribution
+     * @param var Variance parameter for the normal distribution
+     *
+     */
+    template<std::size_t n_rows, std::size_t n_cols>
+    struct NormalInitializer : public Initializer {
+    public:
+      NormalInitializer(const double mean, const double var)
+        : m_mean(mean),
+          m_var(var),
+          m_gaussd(boost::normal_distribution<>(mean, var)) {}
+      MatrixXdType operator()() override {
+        boost::variate_generator<boost::mt19937&,
+                                 boost::normal_distribution<>> gaussvars(rng, m_gaussd);
+        return MatrixXd::Zero(n_rows, n_cols)
+          .unaryExpr([&](double t){ return gaussvars(); });
+      }
+
+    private:
+      double m_mean;
+      double m_var;
+      boost::normal_distribution<double> m_gaussd;
+    };
   }
-};
-
-template<std::size_t n_rows, std::size_t n_cols>
-struct UniformInitializer : public Initializer {
-public:
-  UniformInitializer(const double lower, const double upper)
-    : m_lower(lower),
-      m_upper(upper),
-      m_unifd(boost::random::uniform_real_distribution<>(lower, upper)) {}
-
-  MatrixXdType operator()() override {
-    return MatrixXd::Zero(n_rows, n_cols)
-      .unaryExpr([&](double t){ return m_unifd(rng); });
-  }
-
-private:
-  double m_lower;
-  double m_upper;
-  boost::random::uniform_real_distribution<double> m_unifd;
-};
-
-template<std::size_t n_rows, std::size_t n_cols>
-struct NormalInitializer : public Initializer {
-public:
-  NormalInitializer(const double mean, const double var)
-    : m_mean(mean),
-      m_var(var),
-      m_gaussd(boost::normal_distribution<>(mean, var)) {}
-  MatrixXdType operator()() override {
-    boost::variate_generator<boost::mt19937&,
-                             boost::normal_distribution<>> gaussvars(rng, m_gaussd);
-    return MatrixXd::Zero(n_rows, n_cols)
-      .unaryExpr([&](double t){ return gaussvars(); });
-  }
-
-private:
-  double m_mean;
-  double m_var;
-  boost::normal_distribution<double> m_gaussd;
-};
+}
 
 #endif
