a5fc0e3dceab4076ab850d084f84ceee9dbef131
Alind Gupta <alindgupta19@gmail.com>
2018-03-27 16:27:15 -0400

    Change setter function names to match new names in hmm.hpp

--- main.cpp
+++ main.cpp
@@ -5,20 +5,20 @@
 
 int main(int main, const char **argv) {
 
-  constexpr int num_hidden = 3;
-  constexpr int num_obs = 2;
-  constexpr int obslen = 3;
+  const int num_hidden = 3;
+  const int num_obs = 2;
+  const int obslen = 3;
 
-  hmm::HMM<num_hidden, num_obs> x;
-
+  hmm::HMM x(num_hidden, num_obs);
+  
   Eigen::MatrixXd t(3,3);
   t << 0.25, 0.75, 0, 0, 0.25, 0.75, 0, 0, 1.0;
-  x.set_tprobs(t);
+  x.transition_matrix(t);
 
   Eigen::MatrixXd e(3,2);
   e << 1.0, 0.0, 0.0, 1.0, 1.0, 0.0;
-  x.set_eprobs(e);
-
+  x.emission_matrix(e);
+  
   Eigen::VectorXd o(3);
   o << 0, 1, 0;
 
