de864ab836bdc6363b3982f1ba79d8692454be13
Alind Gupta <alindgupta19@gmail.com>
2018-03-26 23:06:49 -0400

    Test forward algorithm from hmm.hpp

--- main.cpp
+++ main.cpp
@@ -5,9 +5,25 @@
 
 int main(int main, const char **argv) {
 
-  // auto x = hmm::initializers::NormalInitializer<10,10>(0.0, 0.1)();
-  hmm::HMM<10, 5> y;
-  Eigen::VectorXd p(5);
-  p << 1, 2, 3, 1, 1;
-  // y.forward(p);
+  constexpr int num_hidden = 3;
+  constexpr int num_obs = 2;
+  constexpr int obslen = 3;
+
+  hmm::HMM<num_hidden, num_obs> x;
+
+  Eigen::MatrixXd t(3,3);
+  t << 0.25, 0.75, 0, 0, 0.25, 0.75, 0, 0, 1.0;
+  x.set_tprobs(t);
+
+  Eigen::MatrixXd e(3,2);
+  e << 1.0, 0.0, 0.0, 1.0, 1.0, 0.0;
+  x.set_eprobs(e);
+
+  Eigen::VectorXd o(3);
+  o << 0, 1, 0;
+
+  auto a = x.forward(o);
+
+  std::cout << a << std::endl;
+
 }
