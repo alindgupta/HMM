eb169757000e9150eff4f04dc53304db60184d17
Alind Gupta <alindgupta19@gmail.com>
2018-04-06 23:33:03 -0400

    Fix documentation

--- src/hmm.hpp
+++ src/hmm.hpp
@@ -8,17 +8,66 @@ using Vector = Eigen::VectorXd;
 using MatrixType = Eigen::Ref<const Eigen::MatrixXd>;
 using VectorType = Eigen::Ref<const Eigen::VectorXd>;
 
+
 namespace hmm {
 
   class HMM {
     
- zas public:
-    HMM(std::size_t, std::size_t);
-    HMM(const MatrixType&, const MatrixType&);
+  public:
     
+    /**
+     * @brief Construct an HMM object 
+     * given the sizes of hidden and observed states.
+     *
+     * Matrices of probabilities are initialized with the inverse of the
+     * matrix size so that all states and emissions have equal probability.
+     *
+     * @param num_hidden Number N of hidden states.
+     * @param num_observed Number K of observed states.
+     */
+    HMM(std::size_t num_hidden, std::size_t num_observed);
+
+    /**
+     * @brief Construct an HMM object with given probability matrices.
+     *
+     * Invokes copy constructors for the Eigen data types.
+     *
+     * @param a square matrix of transition probabilities (H x H)
+     * @param a matrix of emission probabilities (H x K)
+     */
+    HMM(const MatrixType& transition_probs, const MatrixType&);
+
+    /**
+     * @brief Construct an HMM object with given probability matrices,
+     * including a vector of initial probabilities.
+     *
+     * @param a square matrix of transition probabilities (H x H)
+     * @param a matrix of emission probabilities (H x K)
+     * @param a vector of initial probabilities (H x 1)
+     *
+     * Invokes copy constructors for the Eigen data types.
+     */
+    HMM(const MatrixType&, const MatrixType&, const VectorType&);
+
+    /**
+     * Calculate forward probabilities for a sequence of observed states.
+     *
+     * @param vector of observed states as ``int`` values
+     * @returns matrix (H x N) of forward probabilities
+     */
     Matrix forward(const VectorType&);
+
+    
     Matrix backward(const VectorType&);
-    void infer(const VectorType&);  // Viterbi algorithm
+
+    /**
+     * Calculate the Viterbi path for a sequence of observed states.
+     * 
+     * @param vector of observed states as ``int`` values
+     * @returns a vector of the most likely sequence of hidden states
+     *   that gave rise to the sequence of observed states provided as argument
+     */ 
+    Vector infer(const VectorType&);
 
     // getters
     Matrix transition_matrix() const;
@@ -36,10 +85,12 @@ namespace hmm {
       Matrix::Constant(m_num_hidden,
                        m_num_hidden,
                        1.0 / m_num_hidden);
+    
     Matrix m_emission_probs =
       Matrix::Constant(m_num_hidden,
                        m_num_observed,
                        1.0 / m_num_observed);
+    
     Vector m_initial_probs =
       Vector::Constant(m_num_hidden,
                        1.0 / m_num_observed);
